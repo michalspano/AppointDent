6cb2e3ce7aa9a7470277d70094b9652b8c722265
spano <spano@student.chalmers.se>
2023-11-04 12:09:50 +0100

    #7 Add initial `Express` entry in `src/index.ts`
    This commit adds a provisional `Express` entry in `src/index.ts` to
    test the `Express` server with the dependecies, namely typescript,
    express, and dotenv.

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express, { type Express, type Request, type Response } from 'express';
+import { config } from 'dotenv';
+
+config(); // init dotenv environment
+
+const app: Express = express();
+const port: string = process.env.PORT ?? '8000';
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from AppointDent!');
+});
+
+app.listen(port, () => {
+  console.log('Hello from AppointDent!');
+  console.log(`Server is running at http://localhost:${port}`);
+});
