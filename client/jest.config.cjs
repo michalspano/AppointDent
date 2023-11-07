f7d51999b6e207d3bbe529197f156dc525c31738
Alexander Säfström <139136798+starkbamse@users.noreply.github.com>
2023-11-07 11:18:24 +0100

    Revert "#9 - Added jest"
    This reverts commit d9ecc1022183c55201ae180005b385d96a445403.

--- client/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports ={
-    verbose: true,
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-    preset:  "solid-jest/preset/browser",
-    testEnvironment: 'jsdom',
-    transform: {
-      "^.+\\.[t|j]sx?$": "babel-jest",
-    },
-  }
