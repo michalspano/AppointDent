b1d427ef864140f0c156420d360d33200015d2c8
spano <spano@student.chalmers.se>
2023-11-07 22:52:46 +0100

    #7 Read `db/schema.sql` in db config
    This ensures that the schema is loaded into the database when the
    service starts. Basically, in case the server is invoked the first
    time or for some reason the table is dropped, the schema will be
    loaded into the database, hence preventing the service from crashing
    due to missing tables.

--- services/session-service/db/config.ts
+++ services/session-service/db/config.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from 'fs';
 import Database from 'better-sqlite3';
 import type { Database as DatabaseType } from 'better-sqlite3';
 
@@ -15,17 +16,28 @@ const options: object = Object.freeze({
 const DB_FILE: string = 'db/sessions.db';
 
 /**
+ * @description the path to the schema file.
+ */
+const schemaFilePath: string = 'db/schema.sql';
+
+/**
  * @description the database instance with the options and the local
- * .db file.
+ * .db file. 
  * Set the journal mode to WAL (Write-Ahead Logging) for better
- * and more optimized performance.
+ * and more optimized performance. For the scope of our project,
+ * there should be no problem with this mode. However, consult
+ * the documentation for more information.
+ * @see https://github.com/WiseLibs/better-sqlite3/blob/master/docs/performance.md
  */
 const database: DatabaseType | undefined = (() => {
   try {
     const db = new Database(DB_FILE, options);
     db.pragma('journal_mode = WAL');
+    // Load the schema file to the database.
+    db.exec(readFileSync(schemaFilePath, 'utf8'));
+
     return db;
-  } catch (error: Error | unknown) { 
+  } catch (error: Error | unknown) {
     return undefined;
   }
 })();
