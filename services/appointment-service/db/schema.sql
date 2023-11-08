6f6c73d9fb57a8e41146e4980d0cc3c9245857f0
spano <spano@student.chalmers.se>
2023-11-08 16:40:03 +0100

    #7 Update `schema.sql` of `appointments-service`

--- services/appointment-service/db/schema.sql
+++ services/appointment-service/db/schema.sql
@@ -1,9 +1,10 @@
--- Schema definitions for the session service database
+-- Schema definitions for the appointment service database
 
 CREATE TABLE IF NOT EXISTS appointments (
     id VARCHAR(64) PRIMARY KEY NOT NULL UNIQUE,
     _dateTime DATETIME NOT NULL, -- date + time combined
-    location VARCHAR(255) NOT NULL,
+    long VARCHAR(255) NOT NULL,
+    lat VARCHAR(255) NOT NULL,
     dentist VARCHAR REFERENCES dentists(email) NOT NULL,
     patient VARCHAR REFERENCES patients(email) NOT NULL
 );
