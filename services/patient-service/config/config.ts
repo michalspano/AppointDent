02694c8725e6e67a7e016e8b9b40bc757bce7b9e
spano <spano@student.chalmers.se>
2023-11-08 15:32:44 +0100

    #7 Add `patients` service (initial)
    This commit adds the `patients` service to the project. The service is
    in its initial state based on the service template.

---
+++ services/patient-service/config/config.ts
@@ -0,0 +1,7 @@
+import express, { type Express } from 'express';
+
+const app: Express = express();
+app.use(express.json()); // for parsing application/json
+const port: string = process.env.PORT ?? '3002';
+
+export { app, port };
---
+++ services/patient-service/db/schema.sql
@@ -0,0 +1,9 @@
+-- Schema definitions for the patient service database
+
+CREATE TABLE IF NOT EXISTS patients (
+    email VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE,
+    pass VARCHAR(255) NOT NULL,
+    birthDate DATE NOT NULL,
+    lName VARCHAR(64) NOT NULL,
+    fName VARCHAR(64) NOT NULL
+);
