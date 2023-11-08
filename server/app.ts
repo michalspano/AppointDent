08f56fb01e15380ae1fc146a37b6213576ae2c90
spano <spano@student.chalmers.se>
2023-11-08 14:58:48 +0100

    #7 Extract spawn of services to a script
    This commit extracts the spawn of services to a script. This script is
    called from the app.ts file when the server is running.

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,6 @@
-import fs from "fs";
 import { config } from 'dotenv';
-import { ChildProcess, spawn } from 'child_process';
-import type { Stats } from 'fs';
 import express, { type Express, type Request, type Response } from 'express';
+import spawnServices from './scripts/spawn_services';
 
 config(); // init dotenv environment
 
@@ -19,57 +17,5 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log('Hello from AppointDent!');
   console.log(`Server is running at http://localhost:${port}`);
+  spawnServices(servicesPath); // spawn services when the server is running
 });
-
-// TODO: extract this to a stand-alone script
-fs.readdir(servicesPath, ((err: NodeJS.ErrnoException | null, services : string[]) => {
-
-  if (err) throw Error(err.message);
-  console.log("Building services...");
-
-  for (let i = 0; i < services.length; i++) {
-    const serviceName: string    = services[i].toLocaleUpperCase();
-    const assumedService: string = servicesPath + "/" + services[i];
-
-    fs.lstat(assumedService, ((err: NodeJS.ErrnoException | null, stats: Stats) => {
-      if (err) throw Error(err.message);
-      if (stats.isDirectory()) {
-        const build_process: ChildProcess = spawn('npm', ['run', 'build'], { cwd: assumedService });
-
-        build_process.stderr!.on('data', (data) => {
-          console.log(`${serviceName} build failed`);
-          throw new Error(data);
-        });
-
-        build_process.on('close', (code: number | null) => {
-          console.log(`${serviceName} build process exited with code ${code}`);
-
-          if (code === 0) {
-            console.log(`${serviceName} build succeeded, spawning...`);
-
-            // Remove PORT from the child process environment
-            const child_env: NodeJS.ProcessEnv = process.env;
-            delete child_env.PORT;
-
-            const child: ChildProcess = spawn('npm', ['run', 'start'], {
-              cwd: assumedService,
-              env: child_env
-            });
-
-            child.stdout!.on('data', (data) => {
-              console.log(`${serviceName}: ${data}`);
-            });
-
-            child.stderr!.on('data', (data) => {
-              console.error(`${serviceName}: ${data}`);
-            });
-
-            child.on('close', (code: number | null) => {
-              console.log(`${serviceName} process exited with code ${code}`);
-            });
-          }
-        });
-      }
-    }));
-  }
-}));
