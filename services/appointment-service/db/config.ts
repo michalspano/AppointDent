1be051124bf17421fb7d3e51c497a333b87548c9
spano <spano@student.chalmers.se>
2023-11-13 15:49:55 +0100

    #6 Add `Postman` test mechanism to `appointment-service`
    This commit, put simply, adds these two scripts:
      - `npm run test` (local testing)
      - `npm run test:ci` (CI testing)
    
    The changes made to the source code are needed to realize these two
    scripts that are needed for testing the `appointment-service`.

--- services/appointment-service/db/config.ts
+++ services/appointment-service/db/config.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'fs';
 import Database from 'better-sqlite3';
 import type { Database as DatabaseType } from 'better-sqlite3';
+import fs from 'fs';
 
 /**
  * @description the options for the database connection.
@@ -12,13 +13,21 @@ const options: object = Object.freeze({
 
 /**
  * @description the name of the database file.
+ * When the test environment is used, the value of
+ * DB_FILE is set to a custom path (typically <service>-test.db).
+ * This way, we can seamlessly switch which local .db file is used.
  */
-const DB_FILE: string = './db/appointments.db';
+const DB_FILE: string = process.env.CUSTOM_DB_PATH ?? './db/appointments.db';
 
 /**
  * @description the path to the schema file.
  */
 const schemaFilePath: string = './db/schema.sql';
+
+/* Verify that DB_FILE exists. If not, create it (just an empty file).
+ * This eliminates the error that occurs when the database file does not exist
+ * on the local machine. */
+if (!fs.existsSync(DB_FILE)) fs.writeFileSync(DB_FILE, '');
 
 /**
  * @description the database instance with the options and the local
