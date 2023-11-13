1a3b5c5611389475cf264a063d3ef717fcc36ab5
alesaf <gussafala@student.gu.se>
2023-11-13 18:56:13 +0100

    #5 - Modified notification schema
    Modified notification schema

--- services/notification-service/db/schema.sql
+++ services/notification-service/db/schema.sql
@@ -1,9 +1,9 @@
 -- Schema definitions for the notification service database
 
 CREATE TABLE IF NOT EXISTS notifications (
-    id VARCHAR(64) PRIMARY KEY NOT NULL UNIQUE,
-    _dateTime DATETIME NOT NULL, -- date + time combined
-    message VARCHAR NOT NULL,    -- ideally, the message should not be empty
-    appointmentId VARCHAR(64) NOT NULL
+    id VARCHAR(64) PRIMARY KEY NOT NULL UNIQUE, -- unique notification id
+    timestamp DATETIME NOT NULL, -- unix timestamp
+    message VARCHAR NOT NULL,    -- notification message
+    email VARCHAR NOT NULL,    -- user email
 );
 
