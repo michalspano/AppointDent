52aff8a24f707fc2fa51cf088ac4daf2135dd8b0
arumeel <kaisa.arumeel@gmail.com>
2023-11-14 21:43:34 +0100

    #5 Removed extra comma

--- services/notification-service/db/schema.sql
+++ services/notification-service/db/schema.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS notifications (
     id VARCHAR(64) PRIMARY KEY NOT NULL UNIQUE, -- unique notification id
     timestamp DATETIME NOT NULL, -- unix timestamp
     message VARCHAR NOT NULL,    -- notification message
-    email VARCHAR NOT NULL,    -- user email
+    email VARCHAR NOT NULL    -- user email
 );
 
