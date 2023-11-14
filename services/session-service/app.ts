61bbfc6b78b2a5e2628c02db94d5ea3f1b01665b
arumeel <kaisa.arumeel@gmail.com>
2023-11-14 21:36:06 +0100

    #30 Add config.ts file, remove express imports

--- services/session-service/app.ts
+++ services/session-service/app.ts
@@ -1,7 +1,5 @@
 import { config } from 'dotenv';
-import { type Request, type Response } from 'express';
 import database from './db/config';
-import { app, port } from './config/config';
 import { mqttClient } from './mqtt/mqtt';
 import path from 'path';
 const SERVICES_PATH = path.basename(path.dirname(__dirname));
@@ -9,12 +7,6 @@ void mqttClient.setup(SERVICES_PATH);
 
 config();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from: AppointDent - Session Service');
-});
-app.listen(port, () => {
-  console.log('AppointDent - Sessions Service');
-  console.log(`Server is running at http://localhost:${port}`);
-  console.log(`Using database: ${database?.name}`);
-  console.log(`Database connection: ${((database?.open) ?? false) ? 'OK' : 'ERROR'}`);
-});
+console.log('AppointDent - Sessions Service');
+console.log(`Using database: ${database?.name}`);
+console.log(`Database connection: ${((database?.open) ?? false) ? 'OK' : 'ERROR'}`);
