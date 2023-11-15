afaded9df2bb35fa88442c1d65f8bb3e3e225cbe
spano <spano@student.chalmers.se>
2023-11-15 22:19:21 +0100

    #26 Handle undefined routes in `appointment-service`

--- services/appointment-service/app.ts
+++ services/appointment-service/app.ts
@@ -4,6 +4,7 @@ import database from './db/config';
 import { app, port } from './config/config';
 import { mqttClient } from './mqtt/mqtt';
 import { basename, dirname } from 'path';
+import type { Request, Response } from 'express';
 
 const SERVICES_PATH: string = basename(dirname(__dirname));
 void mqttClient.setup(SERVICES_PATH);
@@ -11,9 +12,12 @@ void mqttClient.setup(SERVICES_PATH);
 config();
 
 // Use the routes defined in routes/index.ts.
-app.use('/api', router);
+app.use('/api/appointments', router);
 
-// TODO: add mechanism to add undefined routes. 
+// Handle undefined routes
+app.use('/api/*', (req: Request, res: Response) => {
+  res.status(404).send('Not found.');
+});
 
 app.listen(port, () => {
   console.log('AppointDent - Appointments Service');
