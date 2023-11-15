2c778a7bd46eb58b206a41ee5537e1fa3de067e1
mariiaz <mariiaz@student.chalmers.se>
2023-11-15 22:24:34 +0100

    #14 implement Dentist update + delete endpoints

--- services/dentist-service/controllers/controllers.ts
+++ services/dentist-service/controllers/controllers.ts
@@ -96,3 +96,62 @@ export const loginController = (req: Request, res: Response): void => {
     res.status(500).json({ message: 'Server Error' });
   }
 };
+
+export const updateDentistController = (req: Request, res: Response): void => {
+  const email = req.params.email;
+  const updatedInfo = req.body;
+
+  if (database === undefined) {
+    res.status(500).send('Database undefined');
+    return;
+  }
+
+  const fieldsToUpdate: string[] = [];
+  const values: any[] = [];
+
+  // Build the SET clause dynamically based on the provided fields in updatedInfo
+  for (const [key, value] of Object.entries(updatedInfo)) {
+    fieldsToUpdate.push(`${key} = ?`);
+    values.push(value);
+  }
+
+  const query = database.prepare(`
+    UPDATE dentists
+    SET ${fieldsToUpdate.join(', ')}
+    WHERE email = ?
+  `);
+
+  try {
+    const result = query.run(...values, email);
+
+    if (result.changes === undefined || result.changes === 0) {
+      res.status(404).json({ message: 'Dentist not found' });
+      return;
+    }
+
+    const updatedDentist = { email, ...updatedInfo };
+    res.json({ updatedDentist });
+  } catch (error) {
+    console.error('Error updating dentist:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export const deleteDentistController = (req: Request, res: Response): void => {
+  const email = req.params.email;
+  if (database === undefined) {
+    res.status(500).send('Database undefined');
+    return;
+  }
+
+  const query = database.prepare('DELETE FROM dentists WHERE email = ?');
+  const result = query.run(email);
+
+  if (result.changes === 0) {
+    // If no rows were affected, then dentist with the given email was not found
+    res.status(404).json({ message: 'Dentist not found' });
+    return;
+  }
+
+  res.json({ message: 'Dentist deleted successfully' });
+};
