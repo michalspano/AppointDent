366611dbffc0244bc0aa621303f80305b40f017e
Agrima Singh <agrima@student.chalmers.se>
2023-11-16 06:30:47 +0100

    #21 Implement routes for patient related endpoints

---
+++ services/patient-service/routes/patientRoute.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import * as patientController from '../controllers/patientController';
+import * as authController from '../controllers/authController';
+
+const router = Router();
+
+router.post('/login', authController.loginController);
+router.post('/register', patientController.registerController);
+router.patch('/:email', patientController.updatePatientController);
+router.delete('/:email', patientController.deletePatientController);
+
+export default router;
