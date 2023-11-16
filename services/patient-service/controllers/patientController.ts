344fb1f4b22631f65827d528de87cfaee4719495
Agrima Singh <agrima@student.chalmers.se>
2023-11-16 12:27:41 +0100

    #21 Refactor structure to add updatePatientController in a separate file

--- services/patient-service/controllers/patientController.ts
+++ services/patient-service/controllers/patientController.ts
@@ -1,48 +1,5 @@
 import { type Request, type Response } from 'express';
 import database from '../db/config';
-import bcrypt from 'bcrypt';
-import type * as BetterSqlite3 from 'better-sqlite3';
-
-export const updatePatientController = (req: Request, res: Response): Response<any, Record<string, any>> => {
-  try {
-    const email = req.params.email;
-    const updatedInfo = req.body;
-
-    if (database === undefined) {
-      sendServerError(res);
-      return res.sendStatus(500);
-    }
-
-    const fieldsToUpdate: string[] = [];
-    const values: any[] = [];
-
-    for (const [key, value] of Object.entries(updatedInfo)) {
-      fieldsToUpdate.push(`${key} = ?`);
-      values.push(value);
-    }
-
-    const query = database.prepare(`
-        UPDATE patients
-        SET ${fieldsToUpdate.join(', ')}
-        WHERE email = ?
-      `);
-
-    const result = query.run(...values, email);
-
-    if (result.changes === undefined || result.changes === 0) {
-      sendNotFound(res, 'Patient not found');
-      return res.sendStatus(500);
-    }
-
-    const updatedPatient = { email, ...updatedInfo };
-
-    return res.json(updatedPatient);
-  } catch (error) {
-    console.error('Error updating patient:', error);
-    sendServerError(res);
-    return res.sendStatus(500);
-  }
-};
 
 export const deletePatientController = (req: Request, res: Response): Response<any, Record<string, any>> => {
   try {
---
+++ services/patient-service/controllers/updatePatientController.ts
@@ -0,0 +1,51 @@
+import { type Request, type Response } from 'express';
+import database from '../db/config';
+
+export const updatePatientController = (req: Request, res: Response): Response<any, Record<string, any>> => {
+  try {
+    const email = req.params.email;
+    const updatedInfo = req.body;
+
+    if (database === undefined) {
+      sendServerError(res);
+      return res.sendStatus(500);
+    }
+
+    const fieldsToUpdate: string[] = [];
+    const values: any[] = [];
+
+    for (const [key, value] of Object.entries(updatedInfo)) {
+      fieldsToUpdate.push(`${key} = ?`);
+      values.push(value);
+    }
+
+    const query = database.prepare(`
+        UPDATE patients
+        SET ${fieldsToUpdate.join(', ')}
+        WHERE email = ?
+      `);
+
+    const result = query.run(...values, email);
+
+    if (result.changes === undefined || result.changes === 0) {
+      sendNotFound(res, 'Patient not found');
+      return res.sendStatus(500);
+    }
+
+    const updatedPatient = { email, ...updatedInfo };
+
+    return res.json(updatedPatient);
+  } catch (error) {
+    console.error('Error updating patient:', error);
+    sendServerError(res);
+    return res.sendStatus(500);
+  }
+};
+
+function sendNotFound (res: Response, message: string): Response {
+  return res.status(404).json({ message });
+}
+
+function sendServerError (res: Response): Response {
+  return res.status(500).json({ message: 'Server Error' });
+}
