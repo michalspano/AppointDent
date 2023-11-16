77531e13a7b32496c321ccb5730352da7495ec8e
mariiaz <mariiaz@student.chalmers.se>
2023-11-16 22:40:12 +0100

    #14 initial implementation of dentist-session services mqtt connection

--- services/dentist-service/controllers/login.controller.ts
+++ services/dentist-service/controllers/login.controller.ts
@@ -1,7 +1,9 @@
 import type { Request, Response } from 'express';
 import database from '../db/config';
-import { createHash } from 'crypto';
 import type { Dentist } from './types';
+import { client } from '../mqtt/mqtt';
+const TOPIC = 'CREATESESSION';
+const RESPONSE_TOPIC = 'SESSION';
 
 export const login = (req: Request, res: Response): void => {
   try {
@@ -18,15 +20,33 @@ export const login = (req: Request, res: Response): void => {
       return;
     }
 
-    const hashedPasswordInput = createHash('sha256').update(pass).digest('hex');
-
-    // Compare the hashed password
-    if (hashedPasswordInput !== dentist.pass) {
-      res.status(401).json({ message: 'Invalid email or password' });
+    if (client === undefined) {
+      res.status(201).json({ message: 'MQTT connection failed' });
       return;
     }
 
-    res.status(200).json({ message: 'Login successful' });
+    const reqId = Math.floor(Math.random() * 1000); // Generates a random integer between 0 and 999
+    client.publish(TOPIC, `${reqId}/${email}/${pass}/*`); // REQID/USERID/SECRET* what is secret?
+    client.subscribe(RESPONSE_TOPIC);
+
+    client.on('message', (topic: string, message: Buffer) => {
+      if (topic === RESPONSE_TOPIC) {
+        if (database === undefined) {
+          res.status(500).send('Database undefined');
+          return;
+        }
+        const result = message.toString();
+        const status = result.split('/')[1].charAt(0); // check
+        console.log(status, result.split('/'));
+        if (status === '1') {
+          // Set the session key in an HTTP-only cookie
+          // res.cookie('sessionKey', sessionKey, { httpOnly: true });
+          res.status(200).json({ message: 'Login successful' });
+        } else {
+          res.status(500).json({ message: 'Login Error' });
+        }
+      }
+    });
   } catch (error) {
     console.error('Error during login:', error);
     res.status(500).json({ message: 'Server Error' });
--- services/dentist-service/controllers/register.controller.ts
+++ services/dentist-service/controllers/register.controller.ts
@@ -1,40 +1,73 @@
 import type { Request, Response } from 'express';
 import database from '../db/config';
-import { createHash } from 'crypto';
 import type { RegistrationRequestBody } from './types';
+import { client } from '../mqtt/mqtt';
+
+const TOPIC = 'AUTHREQ';
+const RESPONSE_TOPIC = 'AUTHRES';
 
 export const register = (req: Request<string, unknown, RegistrationRequestBody>, res: Response): void => {
   try {
     const { email, pass, fName, lName, clinicCountry, clinicCity, clinicStreet, clinicHouseNumber, clinicZipCode, picture } = req.body;
 
-    // Hash password before storing
-    const hashedPassword = createHash('sha256').update(pass).digest('hex');
-    if (database === undefined) {
-      res.status(500).send('Database undefined');
+    if (client === undefined) {
+      res.status(201).json({ message: 'MQTT connection failed' });
       return;
     }
+    const reqId = Math.floor(Math.random() * 1000); // Generates a random integer between 0 and 999
+    client.publish(TOPIC, `${reqId}/${email}/${pass}*`); // REQID/USERID/SECRET*
+    client.subscribe(RESPONSE_TOPIC);
 
-    const query = database.prepare(`
-        INSERT INTO dentists 
-        (email, pass, fName, lName, clinic_country, clinic_city, clinic_street, clinic_house_number, clinic_zipcode, picture) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-      `);
+    client.on('message', (topic: string, message: Buffer) => {
+      if (topic === RESPONSE_TOPIC) {
+        if (database === undefined) {
+          res.status(500).send('Database undefined');
+          return;
+        }
+        const result = message.toString();
+        const status = result.split('/')[1].charAt(0); // check
+        console.log(status, result.split('/'));
+        if (status === '1') {
+        // Authorization successful'
 
-    query.run(email, hashedPassword, fName, lName, clinicCountry, clinicCity, clinicStreet, clinicHouseNumber, clinicZipCode, picture);
+          // Check if the email is already registered
+          const checkEmailQuery = database.prepare(`
+          SELECT COUNT(*) as count
+          FROM dentists
+          WHERE email = ?
+        `);
 
-    const createdDentist = {
-      email,
-      fName,
-      lName,
-      clinicCountry,
-      clinicCity,
-      clinicStreet,
-      clinicHouseNumber,
-      clinicZipCode,
-      picture
-    };
+          const emailCheckResult = checkEmailQuery.get(email) as { count: number };
+          // check if email is already registered before inserting
+          if (emailCheckResult !== null && emailCheckResult.count > 0) {
+            res.status(500).json({ message: 'Email is already registered' });
+          } else {
+            const query = database.prepare(`
+            INSERT INTO dentists 
+            (email, pass, fName, lName, clinic_country, clinic_city, clinic_street, clinic_house_number, clinic_zipcode, picture) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+          `);
+            query.run(email, pass, fName, lName, clinicCountry, clinicCity, clinicStreet, clinicHouseNumber, clinicZipCode, picture);
 
-    res.status(201).json(createdDentist);
+            const createdDentist = {
+              email,
+              fName,
+              lName,
+              clinicCountry,
+              clinicCity,
+              clinicStreet,
+              clinicHouseNumber,
+              clinicZipCode,
+              picture
+            };
+
+            return res.status(201).json(createdDentist);
+          }
+        } else {
+          return res.status(500).json({ message: 'Authorization failed' });
+        }
+      }
+    });
   } catch (error) {
     console.error('Error registering dentist:', error);
     res.status(500).json({ message: 'Server Error' });
