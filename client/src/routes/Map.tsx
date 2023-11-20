b71af44cdbb8cc7eefcd72414ba7b50a9a774fa5
alesaf <gussafala@student.gu.se>
2023-11-21 00:14:37 +0100

    #35 - Added dentist card, formatting, added demo picture, styling
    I have finalized the map. I have created helper function that upon invocation, will get all dentists from the dentist API and allow the user find the dentist that they want to visit. Later on, it could be good to perhaps make it so that we get the closest dentists. But this will do for now as an MVP.

--- client/src/routes/Map.tsx
+++ client/src/routes/Map.tsx
@@ -10,21 +10,17 @@ export default function Map (): JSX.Element {
     /**
      * Initialize map with gothenburg coordinates
      */
-    const map = new leaflet.Map('map', {
-      center: new leaflet.LatLng(57.708870, 11.974560),
-      zoom: 12
-    })
+    const map = new leaflet.Map('map', { center: new leaflet.LatLng(57.708870, 11.974560), zoom: 12 })
     /**
      * Add map picture
      */
-    leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
-    }).addTo(map)
-
+    leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+    /**
+     * Add all dentists to the map
+     */
     void addDentistsToMap(map)
   })
   return <>
   <div id="map" style={{ height: '100vh', width: '100%' }} />
-
   </>
 }
