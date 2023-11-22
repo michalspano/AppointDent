d8467eee0b6398b4b20fded02907624bb832229d
spano <spano@student.chalmers.se>
2023-11-22 11:46:06 +0100

    #53 Resolve hanging processes

--- server/broker.ts
+++ server/broker.ts
@@ -2,7 +2,8 @@ import Aedes from 'aedes';
 import type { Server } from 'net';
 import { createServer } from 'net';
 
-const port: number = 1883;
+const rawPort: string | undefined = process.env.LOCAL_BROKER_PORT;
+const port: number = rawPort ? parseInt(rawPort) : 1883;
 const aedes: Aedes = new Aedes();
 const server: Server = createServer(aedes.handle);
 
