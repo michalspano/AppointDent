3973961b53fbd83726b02f15e7b18c3890fbcae3
alesaf <gussafala@student.gu.se>
2023-11-22 18:48:41 +0100

    #52 - Refactored proxy
    Refactored proxy in accordance to the PR comments. Particularly the creation of proxies was changed.

--- server/proxy/proxy.ts
+++ server/proxy/proxy.ts
@@ -1,16 +1,34 @@
 import { type NextFunction, type Request, type Response } from 'express';
 import httpProxy from 'http-proxy';
-
-const proxyTargets: Record<string, string> = {
+type ProxyMap = Record<string, httpProxy>;
+type ProxyTargets = Record<string, string>;
+/**
+ * Supposing that the services may be deployed to remote destinations (that are not localhost),
+ * we will need, in the future, to enable these urls to be modified.
+ * For that, an .env file could be used, so these values that are "hard-coded"
+ * now will be the "fallback" (default) values.
+ */
+const proxyTargets: ProxyTargets = {
   patients: 'http://localhost:3002',
   appointments: 'http://localhost:3003',
   notifications: 'http://localhost:3004',
   dentists: 'http://localhost:3005'
 };
-const proxies: Record<string, httpProxy> = {};
-for (const key in proxyTargets) {
-  console.log(key);
-  proxies[key] = httpProxy.createProxyServer({ target: proxyTargets[key], ws: false });
+
+const proxies: ProxyMap = constructProxies(proxyTargets);
+
+/**
+ * Converts the listed proxy targets into proxy servers
+ * @param targets proxy targets
+ * @returns proxy map
+ */
+function constructProxies (targets: ProxyTargets): ProxyMap {
+  const newProxies: ProxyMap = {};
+  for (const key in proxyTargets) {
+    console.log(key);
+    newProxies[key] = httpProxy.createProxyServer({ target: proxyTargets[key], ws: false });
+  }
+  return newProxies;
 }
 
 export function routeProxy (req: Request, res: Response, next: NextFunction): void {
