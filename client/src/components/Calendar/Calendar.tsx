4d4e1ad34e647c693c6dcece4a7c5f6e1264caf9
arumeel <kaisa.arumeel@gmail.com>
2023-11-23 15:36:19 +0100

    #46 Calendar page integration with backend
    Create appointment works, when an appointment is patched, the calendar updates accordingly, delete appointment works.
    
    NB! Dentist email is hardcoded to be "dentist@gmail.com" To be changed!

--- client/src/components/Calendar/Calendar.tsx
+++ client/src/components/Calendar/Calendar.tsx
@@ -6,22 +6,17 @@ import './Calendar.css'
 import { type Appointment } from '../../utils/types'
 import { type EventContentArg, type EventClickArg } from '@fullcalendar/core'
 import ConfirmationPopup from './DeleteConfirmation'
+import { Api } from '../../utils/api'
 
 export default function DentistCalendar (): JSX.Element {
   const [showForm, setShowForm] = createSignal<boolean>(false)
   const [showConfirmation, setShowConfirmation] = createSignal<boolean>(false)
   const [selectedEventId, setSelectedEventId] = createSignal<string | null>(null)
+  const [slots, setSlots] = createSignal<Appointment[]>([]) // Use state to hold appointments
   const calendarRef: { current: Calendar | null } = { current: null }
 
   // Temporary array of appointment, will be modified once integrated with backend.
-  const slots: Appointment[] = [
-    {
-      id: '1',
-      title: 'testevent',
-      start: '2023-11-18T10:31',
-      end: '2023-11-18T12:31'
-    }
-  ]
+
   const [newAppointment, setNewAppointment] = createSignal<Appointment>({
     id: '',
     title: '',
@@ -29,16 +24,64 @@ export default function DentistCalendar (): JSX.Element {
     end: ''
   })
 
+  async function handleFormSubmit (e: any): Promise<void> {
+    e.preventDefault()
+    try {
+      await addAppointment(newAppointment())
+    } catch (error) {
+      console.error('Error adding appointment:', error)
+      // Handle the error, show a message to the user, etc.
+    }
+  }
+
+  function parseDateStringToInteger (dateString: string): number {
+    // Parse the date string into a Date object
+    const date = new Date(dateString)
+    // Get the timestamp in milliseconds
+    const timestamp = date.getTime()
+    // Convert the timestamp to seconds (integer)
+    const timestampInSeconds = Math.floor(timestamp / 1000)
+
+    return timestampInSeconds
+  }
+
+  async function fetchAppointments (): Promise<void> {
+    try {
+      const response = await Api.get('/appointments/dentists/dentist@gmail.com/') // NB! replace with actual email
+      const appointments = response.data // Assuming your API response contains the appointments
+      const formattedAppointments = appointments.map((appointment: any) => ({
+        id: appointment.id,
+        title: appointment.patientId !== null ? appointment.patientId : '', // Make sure to replace 'title' with the actual property name
+        start: new Date(appointment.start_timestamp * 1000).toISOString().slice(0, 16),
+        end: new Date(appointment.end_timestamp * 1000).toISOString().slice(0, 16)
+      }))
+
+      setSlots(formattedAppointments)
+    } catch (error) {
+      console.error('Error fetching appointments:', error)
+    }
+  }
   /**
- * Adds a new appointment to the slots array and updates the calendar.
+ * Adds a new appointment to the database and updates the calendar.
  *
  * @param {Appointment} appointment - The appointment to be added.
  */
-  function addAppointment (appointment: Appointment): void {
-    slots.push(appointment)
-
-    // Update the FullCalendar instance with the new appointment
-    calendarRef.current?.addEvent(appointment)
+  async function addAppointment (appointment: Appointment): Promise<void> {
+    try {
+      const startTimestamp = parseDateStringToInteger(newAppointment().start)
+      const endTimestamp = parseDateStringToInteger(newAppointment().end)
+      const formattedAppointment = {
+        start_timestamp: startTimestamp,
+        end_timestamp: endTimestamp,
+        dentistId: 'dentist@gmail.com' // Replace with actual dentist email from local storage
+      }
+      await Api.post('/appointments', formattedAppointment)
+        .then(() => {
+          calendarRef.current?.addEvent(appointment)
+        })
+    } catch (error) {
+      throw new Error('Something went wrong, try again.')
+    }
     // Reset the new appointment state
     setNewAppointment({
       id: '',
@@ -55,16 +98,23 @@ export default function DentistCalendar (): JSX.Element {
  *
  * @param appointmentId - The id of the appointment to be removed.
  */
-  function deleteAppointment (appointmentId: string): void {
-    const index = slots.findIndex((appointment) => appointment.id === appointmentId)
-    if (index !== -1) {
-      slots.splice(index, 1)
-      calendarRef.current?.getEventById(appointmentId)?.remove()
+  async function deleteAppointment (appointmentId: string): Promise<void> {
+    try {
+      await Api.delete(`/appointments/${appointmentId}`).then(() => {
+        calendarRef.current?.getEventById(appointmentId)?.remove()
+        // Add any additional actions you want to perform after successful deletion
+        console.log('Appointment deleted successfully')
+      })
+    } catch (error) {
+      console.error('Error deleting appointment:', error)
+      // Handle the error, show a message to the user, etc.
+      throw new Error('Error deleting appointment')
     }
   }
 
   // Effect to initialize FullCalendar and manage its lifecycle
-  createEffect(() => {
+  createEffect(async () => {
+    await fetchAppointments()
     const calendarEl: HTMLElement | null = document.getElementById('calendar')
     if (calendarEl != null) {
       // Create a FullCalendar instance
@@ -96,20 +146,26 @@ export default function DentistCalendar (): JSX.Element {
         eventClassNames: function (arg: EventContentArg) {
           return arg.event.title === '' ? 'unbooked-event' : 'booked-event'
         },
-        events: [...slots],
+        events: slots().map((appointment) => ({
+          id: appointment.id, // Use your own appointment ID
+          title: appointment.title,
+          start: appointment.start,
+          end: appointment.end
+        })),
         eventClick: ({ event }: EventClickArg) => {
           setSelectedEventId(event.id) // Store the selected event ID
           setShowConfirmation(true)
         }
       })
 
+      setInterval(async () => {
+        await fetchAppointments()
+        const events = [...slots()]
+        calendar.setOption('events', events)
+      }, 5000)
+
       calendarRef.current = calendar
       calendar.render()
-
-      // Use onCleanup to destroy the calendar when the component unmounts
-      onCleanup(() => {
-        calendar.destroy()
-      })
     }
   })
 
@@ -123,7 +179,10 @@ export default function DentistCalendar (): JSX.Element {
             id='eventForm'
             onSubmit={(e) => {
               e.preventDefault()
-              addAppointment(newAppointment())
+              handleFormSubmit(e).catch((error) => {
+                console.error('Error adding appointment:', error)
+                // Handle the error, show a message to the user, etc.
+              })
             }}
           >
             <div class='flex flex-col'>
@@ -180,17 +239,21 @@ export default function DentistCalendar (): JSX.Element {
 
       {showConfirmation() && (
           <ConfirmationPopup
-            onConfirm={() => {
-              const eventId = selectedEventId()
-              if (eventId !== null) {
-                deleteAppointment(eventId)
+          onConfirm={() => {
+            const eventId = selectedEventId()
+            if (eventId !== null) {
+              deleteAppointment(eventId).then(() => {
                 setShowConfirmation(false)
-              }
-            }}
-              onCancel={() => {
-                setShowConfirmation(false)
-              }}
-          />
+              }).catch((error) => {
+                console.error('Error deleting appointment:', error)
+                // Handle the error, show a message to the user, etc.
+              })
+            }
+          }}
+          onCancel={() => {
+            setShowConfirmation(false)
+          }}
+        />
       )}
     </>
   )
