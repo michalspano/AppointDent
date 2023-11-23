10c548c847520cb00af9502fc9682cedf0f1670a
Agrima Singh <agrima@student.chalmers.se>
2023-11-23 12:04:57 +0100

    #21 Integrate authController with session service and MQTT for patient-service

--- services/patient-service/controllers/authController.ts
+++ services/patient-service/controllers/authController.ts
@@ -1,48 +1,68 @@
-import { type Request, type Response } from 'express';
+import type { Request, Response } from 'express';
 import database from '../db/config';
-import { sendUnauthorized, sendServerError, sendSuccess } from './controllerUtils';
+import { client } from '../mqtt/mqtt';
+import { sendServerError, sendCreated, sendSuccess } from './controllerUtils';
 
-interface Patient {
-  email: string
-  pass: string
+const TOPIC = 'CREATESESSION';
+const RESPONSE_TOPIC = 'SESSION';
+const TIMEOUT = 10000;
+
+async function getServiceResponse (reqId: string, RESPONSE_TOPIC: string, isLoginFlow: boolean = false): Promise<string> {
+  return await new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      client?.unsubscribe(RESPONSE_TOPIC);
+      reject(new Error('MQTT timeout'));
+    }, TIMEOUT);
+    const eventHandler = (topic: string, message: Buffer): void => {
+      if (topic === RESPONSE_TOPIC) {
+        if (message.toString().startsWith(`${reqId}/`)) {
+          clearTimeout(timeout);
+          client?.unsubscribe(topic);
+          client?.removeListener('message', eventHandler);
+          isLoginFlow
+            ? resolve(message.toString().split('/')[1])
+            : resolve(message.toString().split('/')[1][0]);
+        }
+      }
+    };
+    client?.subscribe(RESPONSE_TOPIC);
+    client?.on('message', eventHandler);
+  });
 }
 
-export const loginController = (req: Request, res: Response): void => {
-  // console.log('Here!!!');
-  try {
-    const { email, pass } = req.body;
-
-    if (database === undefined) {
-      sendServerError(res);
-      return;
-    }
-
-    const patient = database.prepare('SELECT * FROM patients WHERE email = ?').get(email) as Patient;
-
-    if (patient === undefined) {
-      sendUnauthorized(res, 'Invalid email or password');
-      return;
-    }
-
-    if (patient.pass !== pass) {
-      sendUnauthorized(res, 'Invalid email or password');
-      return;
-    }
-
-    /* bcrypt.compare(pass, patient.pass, (compareError: Error | undefined, passwordMatch: boolean) => {
-      if (compareError != null) {
-        sendServerError(res); return;
-      }
-
-      if (!passwordMatch) {
-        sendUnauthorized(res, 'Invalid email or password'); return;
-      }
-      */
-
-    sendSuccess(res, 'Login successful');
-  } catch (error) {
-    console.error('Error during login:', error);
-    sendServerError(res);
-    console.log('Here!!!');
+export const loginController = async (req: Request, res: Response): Promise<Response<any, Record<string, any>>> => {
+  const { email, pass } = req.body;
+  if (database === undefined) {
+    return sendServerError(res, 'Database undefined');
   }
+  if (client === undefined) {
+    return sendCreated(res, { message: 'MQTT connection failed' });
+  }
+
+  const reqId = Math.floor(Math.random() * 1000);
+  client.publish(TOPIC, `${reqId}/${email}/${pass}/*`);
+  client.subscribe(RESPONSE_TOPIC);
+
+  try {
+    const mqttResult = await getServiceResponse(reqId.toString(), RESPONSE_TOPIC);
+
+    if (mqttResult === '0') {
+      return sendCreated(res, { message: 'Unable to authorize' });
+    }
+
+    if (mqttResult !== undefined && mqttResult.length === 1 && mqttResult === '0') {
+      return sendServerError(res, 'Email or password is incorrect');
+    } else {
+      res.cookie('sessionKey', mqttResult, { httpOnly: true });
+      return sendSuccess(res, 'Login successful');
+    }
+  } catch (error) {
+    return sendServerError(res, 'Service Timeout');
+  } finally {
+    client?.unsubscribe(RESPONSE_TOPIC);
+  }
+};
+
+export const loginPatientWrapper = (req: Request, res: Response): void => {
+  void loginController(req, res);
 };
