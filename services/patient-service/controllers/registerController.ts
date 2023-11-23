5b1317c84a65994a98175fef59e43d86f454ca42
Agrima Singh <agrima@student.chalmers.se>
2023-11-23 13:33:44 +0100

    #21 Add helper module and update the registerController

--- services/patient-service/controllers/registerController.ts
+++ services/patient-service/controllers/registerController.ts
@@ -1,107 +1,88 @@
-import { type Request, type Response } from 'express';
+import type { Request, Response } from 'express';
 import database from '../db/config';
 import { client } from '../mqtt/mqtt';
-import { sendServerError, sendCreated, sendBadRequest } from './controllerUtils';
-
-interface PatientRegistrationRequest {
-  email: string
-  pass: string
-  birthDate: string
-  fName: string
-  lName: string
-}
-
-interface PatientRegistrationResponse {
-  email: string
-  birthDate: string
-  fName: string
-  lName: string
-}
+import { getServiceResponse } from './helper';
+import {
+  sendCreated,
+  sendServerError,
+  sendUnauthorized,
+  sendBadRequest
+} from './controllerUtils';
 
 const TOPIC = 'INSERTUSER';
 const RESPONSE_TOPIC = 'INSERTUSERRES';
-const TIMEOUT = 10000;
 
-export const registerController = async (req: Request<Record<string, unknown>, unknown, PatientRegistrationRequest>, res: Response<PatientRegistrationResponse>): Promise<void> => {
-  try {
-    const { email, pass, birthDate, fName, lName } = req.body;
+export const registerController = async (req: Request, res: Response): Promise<Response<any, Record<string, any>>> => {
+  const {
+    email,
+    pass,
+    birthDate,
+    lName,
+    fName
+  } = req.body;
 
-    if ((database == null) || (client == null)) {
-      sendServerError(res, 'Database or MQTT connection is undefined');
-      return;
-    }
-
-    const reqId = Math.floor(Math.random() * 10000).toString();
-    const mqttRequestMessage = `${reqId}/${email}/${pass}/*`;
-
-    client.publish(TOPIC, mqttRequestMessage);
-    client.subscribe(RESPONSE_TOPIC);
-
-    try {
-      const mqttResult = await new Promise<string>((resolve, reject) => {
-        const timeout = setTimeout(() => {
-          reject(new Error('MQTT timeout'));
-        }, TIMEOUT);
-        const eventHandler = (topic: string, message: Buffer): void => {
-          if (topic === RESPONSE_TOPIC) {
-            if (message.toString().startsWith(`${reqId}/`)) {
-              clearTimeout(timeout);
-              client?.unsubscribe(RESPONSE_TOPIC);
-              client?.removeListener('message', eventHandler);
-              resolve(message.toString().split('/')[1][0]);
-            }
-          }
-        };
-        client?.on('message', eventHandler);
-      });
-
-      if (mqttResult === '0') {
-        sendBadRequest(res, 'Unable to authorize');
-        return;
-      }
-    } catch (error) {
-      sendServerError(res, 'Service Timeout');
-      return;
-    } finally {
-      client?.unsubscribe(RESPONSE_TOPIC);
-    }
-
-    if (checkEmailRegistered(email)) {
-      sendBadRequest(res, 'Email is already registered');
-      return;
-    }
-
-    const query = database.prepare(`
-      INSERT INTO patients 
-      (email, pass, birthDate, fName, lName) 
-      VALUES (?, ?, ?, ?, ?)
-    `);
-
-    query.run(email, pass, birthDate, fName, lName);
-
-    const createdPatient = {
-      email,
-      birthDate,
-      fName,
-      lName
-    };
-
-    sendCreated(res, createdPatient);
-  } catch (error) {
-    sendServerError(res, 'Error registering patient');
+  // To verify if required dependencies are defined
+  if (database === undefined) {
+    return sendServerError(res, 'Database undefined');
   }
+  if (client === undefined) {
+    return sendUnauthorized(res, 'MQTT connection failed');
+  }
+
+  // To generate a random reqId
+  const reqId = Math.floor(Math.random() * 1000);
+
+  // To publish registration information to MQTT topic
+  client.publish(TOPIC, `${reqId}/${email}/${pass}/*`);
+  client.subscribe(RESPONSE_TOPIC);
+
+  try {
+    // To wait for MQTT response
+    const mqttResult = await getServiceResponse(reqId.toString(), RESPONSE_TOPIC);
+
+    // To handle unsuccessful authorization
+    if (mqttResult === '0') {
+      return sendUnauthorized(res, 'Unable to authorize');
+    }
+  } catch (error) {
+    return sendServerError(res, 'Service Timeout');
+  }
+
+  // To check if the email is already registered
+  if (checkEmailRegistered(email)) {
+    return sendBadRequest(res, 'Email is already registered');
+  }
+
+  // To insert user into the database
+  const query = database.prepare(`
+    INSERT INTO patients 
+    (email, pass, birthDate, lName, fName) 
+    VALUES (?, ?, ?, ?, ?)
+  `);
+  query.run(email, pass, birthDate, lName, fName);
+
+  // To return success response with created user's information
+  const createdPatient = {
+    email,
+    birthDate,
+    lName,
+    fName
+  };
+  return sendCreated(res, createdPatient);
 };
 
+// To check if the email is already registered
 function checkEmailRegistered (email: string): boolean {
   const checkEmailQuery = database?.prepare(`
     SELECT COUNT(*) as count
     FROM patients
     WHERE email = ?
   `);
-
   const emailCheckResult = checkEmailQuery?.get(email) as { count: number };
+
   return emailCheckResult !== null && emailCheckResult.count > 0;
 };
+
 export const registerPatientWrapper = (req: Request, res: Response): void => {
   void registerController(req, res);
 };
