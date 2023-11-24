84c6e0d2f03df2b893135afe4a82eead06acbdcd
spano <spano@student.chalmers.se>
2023-11-24 16:27:40 +0100

    #49 Add `cookie-parser` to `Express` config

--- services/appointment-service/config/config.ts
+++ services/appointment-service/config/config.ts
@@ -1,9 +1,11 @@
 import express, { type Express } from 'express';
+import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 
 const app: Express = express();
 app.use(morgan('dev')); // Add morgan HTTP request logger.
 app.use(express.json()); // for parsing application/json
+app.use(cookieParser()); // Add cookie parser.
 app.use(express.urlencoded({ extended: true }));
 
 const port: string = process.env.PORT ?? '3003';
