bd783f4d752c644739d34a9239340fd212d2b345
OmidKhodaparast2002 <guskhodom@student.gu.se>
2023-11-24 15:45:11 +0100

    #19 construct notification service
    I have finished the code. I just have to add a couple of postman tests. Moreover, I have used a middleware for authorisation. Status: incomplete

--- services/notification-service/config/config.ts
+++ services/notification-service/config/config.ts
@@ -1,11 +1,16 @@
 import express, { type Express } from 'express';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import { authoriseUser } from '../middlewares/authorisation';
 
 const app: Express = express();
 
 app.use(morgan('dev'));
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use('/:id', authoriseUser);
+app.use('/:email', authoriseUser);
 
 const port: string = process.env.PORT ?? '3004';
 
