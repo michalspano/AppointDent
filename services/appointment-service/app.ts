2122c7183fd53f153441672ae90ad6f9d6e65ab4
spano <spano@student.chalmers.se>
2023-11-25 14:09:22 +0100

    #49 Resolve dependency mismatch; polish code

--- services/appointment-service/app.ts
+++ services/appointment-service/app.ts
@@ -1,21 +1,30 @@
-import { config } from 'dotenv';
 import router from './routes';
+import { config } from 'dotenv';
+import app from './config/config';
 import database from './db/config';
-import { app, port } from './config/config';
-import { mqttClient } from './mqtt/mqtt';
 import { basename, dirname } from 'path';
+import { mqttClient } from './mqtt/mqtt';
 import type { Request, Response } from 'express';
 
 const SERVICES_PATH: string = basename(dirname(__dirname));
 void mqttClient.setup(SERVICES_PATH);
 
-config();
+config(); // read .env file
+
+/**
+ * @description The port on which the server is running.
+ * @see process.env.PORT
+ */
+const port: string = process.env.PORT ?? '3003';
 
 // Use the routes defined in routes/index.ts.
 app.use('/', router);
+
+// TODO: examine the feasibility of this route and if it is needed.
 app.get('/', (req, res) => {
   res.sendStatus(200);
 });
+
 // Handle undefined routes
 app.use('*', (req: Request, res: Response) => {
   res.status(404).send('Not found.');
