972de3a68c550ac8ddc7e05481b5eb3330e1f5ed
Agrima Singh <agrima@student.chalmers.se>
2023-11-26 20:53:09 +0100

    #23 Link the notification icon in Navbar to the notifications page

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -56,39 +56,41 @@ export default function Navbar (): JSX.Element {
                 </div>
 
                 </div>
-            <div class="flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div class="flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <a href="/notifications">
                 <button onClick={toggleNotification} type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none">
-                <span class="absolute -inset-1.5"></span>
-                <span class="sr-only">View notifications</span>
-                <svg class="h-6 w-6 notification" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                </svg>
+                    <span class="absolute -inset-1.5"></span>
+                    <span class="sr-only">View notifications</span>
+                    <svg class="h-6 w-6 notification" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                    </svg>
                 </button>
-                <div class="inset-y-0 right-0 absolute top-10">
-                    <Show when={notify()}>
-                        <div class={ fadeIn() ? 'notification fade-in-element' : fadeOut() ? 'notification fade-out-element' : 'notification' }>
-                            <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                                <p class="block px-4 py-2 text-sm text-primary">No notifications!</p>
-                            </div>
+            </a>
+            <div class="inset-y-0 right-0 absolute top-10">
+                <Show when={notify()}>
+                    <div class={fadeIn() ? 'notification fade-in-element' : fadeOut() ? 'notification fade-out-element' : 'notification'}>
+                        <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                            <p class="block px-4 py-2 text-sm text-primary">No notifications!</p>
                         </div>
-                    </Show>
-                </div>
-            </div>
-            </div>
-        </div>
-
-    </nav>
-    <Show when={hamburger()}>
-        <div class={ slideIn() ? 'slide-in-element bg-primary zUnder' : slideOut() ? 'slide-out-element bg-primary zUnder' : 'bg-primary zUnder' }>
-            <div id="">
-                <div class="space-y-1 px-2 pb-3 pt-2">
-                <For each={routes}>{(route) =>
-                        <a href={route.href} class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">{route.name}</a>
-
-                    }</For>
-                </div>
+                    </div>
+                </Show>
             </div>
         </div>
-        </Show>
-    </>
+                    </div>
+                </div>
+
+            </nav>
+            <Show when={hamburger()}>
+                <div class={ slideIn() ? 'slide-in-element bg-primary zUnder' : slideOut() ? 'slide-out-element bg-primary zUnder' : 'bg-primary zUnder' }>
+                    <div id="">
+                        <div class="space-y-1 px-2 pb-3 pt-2">
+                        <For each={routes}>{(route) =>
+                                <a href={route.href} class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">{route.name}</a>
+
+                            }</For>
+                        </div>
+                    </div>
+                </div>
+                </Show>
+            </>
 }
