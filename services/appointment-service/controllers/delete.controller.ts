dc8d9b5f4414cae49bc22b3625bbed1febaf9eed
spano <spano@student.chalmers.se>
2023-11-27 12:42:06 +0100

    #49 Protect `DELETE/:id` (appointments)
    Furthermore, `DELETE /` is deleted, because it can be deemed as a
    dangerous operation. It is not possible to delete all appointments
    anymore (unless a new `Admin` role is introduced).

--- services/appointment-service/controllers/delete.controller.ts
+++ services/appointment-service/controllers/delete.controller.ts
@@ -6,57 +6,98 @@
  */
 
 import database from '../db/config';
+import { client } from '../mqtt/mqtt';
+import * as utils from '../utils';
 import type { Request, Response } from 'express';
 import { type Statement } from 'better-sqlite3';
+import { SessionResponse, type AsyncResObj, UserType, type WhoisResponse, type Appointment } from '../types/types';
 
-// Delete all appointment entries; preserve the schema.
-// TODO: ensure that only a **dentist** can delete **own** appointments.
-export const deleteAllAppointments = (req: Request, res: Response): Response<any, Record<string, any>> => {
+const TOPIC: string = utils.MQTT_PAIRS.whois.req;
+const RESPONSE_TOPIC: string = utils.MQTT_PAIRS.whois.res;
+
+/**
+ * @description the controller that deletes an appointment. This controller is protected
+ * and only a dentist can delete an **own** appointment (i.e., an appointment that they are
+ * assigned to). Furthermore, the dentist must be logged in and have a valid session. All
+ * other requests shall be rejected.
+ *
+ * @see whoisByToken
+ * @see WhoisResponse
+ *
+ * @param req the request object
+ * @param res the response object
+ * @returns Promise that resolves to an asynchronous response JSON-like object.
+ */
+export const deleteAppointment = async (req: Request, res: Response): AsyncResObj => {
   if (database === undefined) {
     return res.status(500).json({
       message: 'Internal server error: database connection failed.'
     });
   }
+
+  if (client === undefined) {
+    return res.status(503).json({
+      message: 'Service unavailable: MQTT connection failed.'
+    });
+  }
+
+  const email: string | undefined = req.body.email;
+  const sessionKey: string | undefined = req.cookies.sessionKey;
+
+  if (sessionKey === undefined || email === undefined) {
+    return res.status(400).json({ message: 'Bad request: missing session key or email.' });
+  }
+
+  // Use the `WHOIS` topic to determine the role of the user.
+  // This, in turn, determines whether the user is allowed to create an appointment.
+  const reqId: string = utils.genReqId();
+  client.publish(TOPIC, `${reqId}/${sessionKey}/*`);
+  client.subscribe(RESPONSE_TOPIC);
 
   try {
-    database.prepare('DELETE FROM appointments').run();
+    const result: WhoisResponse = await utils.whoisByToken(
+      reqId.toString(),
+      RESPONSE_TOPIC
+    );
+
+    if (result.status !== SessionResponse.Success) {
+      return res.status(401).json({ message: 'Unauthorized: invalid session.' });
+    }
+
+    // Restrict access to dentists only.
+    if (result.type !== UserType.Dentist) {
+      return res.status(403).json({
+        message: 'Forbidden: only dentists can delete appointments.'
+      });
+    }
   } catch (err: Error | unknown) {
-    return res.status(500).json({
-      message: 'Internal server error: query failed.'
-    });
+    return res.status(504).json({ message: 'Service timeout: unable to verify session.' });
   }
 
-  // Deletion was successful.
-  return res.status(204).end();
-};
-
-// Delete an appointment by id.
-export const deleteAppointment = (req: Request, res: Response): Response<any, Record<string, any>> => {
-  if (database === undefined) {
-    return res.status(500).json({
-      message: 'Internal server error: database connection failed.'
-    });
-  }
-
-  const id: string = req.params.id;
-  let objToDelete: unknown;
+  const id: string = req.params.id; // id of the appointment to delete
+  let objToDelete: Appointment | undefined;
   try {
     objToDelete = database
       .prepare('SELECT * FROM appointments WHERE id = ?')
-      .get(id);
+      .get(id) as Appointment;
   } catch (err: Error | unknown) {
     return res.status(500).json({
       message: 'Internal server error: query failed.'
     });
   }
 
+  // Appointment not found.
   if (objToDelete === undefined) {
-    return res.status(404).json({
-      message: `Appointment with id ${id} not found.`
-    });
+    return res.status(404).json({ message: 'Not found.' });
   }
 
-  // Object is found, process to delete it.
+  // Ensure that the dentist is deleting an appointment that they are assigned to.
+  // That is, the dentist is in the `dentistId` field of the appointment.
+  if (objToDelete.dentistId !== email) {
+    return res.status(403).json({ message: 'Forbidden: disallowed operation.' });
+  }
+
+  // All steps have been completed, proceed with deleting the appointment...
   const stmt: Statement = database.prepare('DELETE FROM appointments WHERE id = ?');
   try {
     stmt.run(id);
@@ -66,8 +107,18 @@ export const deleteAppointment = (req: Request, res: Response): Response<any, Re
     });
   }
 
-  // Everything went well, return the deleted object.
   return res.status(200).json(objToDelete);
 };
 
-export default deleteAppointment;
+/**
+ * @description wrapper function for the deleteAppointment function
+ * that resolves the asynchronous nature of the function being wrapped.
+ *
+ * @param req the request object
+ * @param res the response object
+ */
+const deleteAppointmentWrapper = (req: Request, res: Response): void => {
+  void deleteAppointment(req, res);
+};
+
+export default deleteAppointmentWrapper;
--- services/appointment-service/routes/index.ts
+++ services/appointment-service/routes/index.ts
@@ -9,15 +9,14 @@ import { Router } from 'express';
 import createAppointment from '../controllers/post.controller';
 import editAppointment from '../controllers/patch.controller';
 import * as getControllers from '../controllers/get.controller';
-import * as deleteControllers from '../controllers/delete.controller';
+import deleteAppointment from '../controllers/delete.controller';
 
 const router: Router = Router();
 
 router.get('/', getControllers.getAllAppointmentsWrapper);
 router.get('/:id', getControllers.getAppointment);
 router.post('/', createAppointment);
-router.delete('/', deleteControllers.deleteAllAppointments);
-router.delete('/:id', deleteControllers.deleteAppointment);
+router.delete('/:id', deleteAppointment);
 router.patch('/:id', editAppointment);
 
 // Relational endpoints.
