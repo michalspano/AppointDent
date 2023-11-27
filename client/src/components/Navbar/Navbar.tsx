eae645a5d0b2ad0df5a571499832a78c9910f57d
arumeel <kaisa.arumeel@gmail.com>
2023-11-27 10:46:17 +0100

    #47 Fix merge conflict

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -5,8 +5,8 @@ import { For, Show } from 'solid-js'
 import { patientRoutes } from './routes'
 import { fadeIn, fadeOut, hamburger, notify, slideIn, slideOut, toggleHamburger, toggleNotification } from './animation'
 import location from '../../assets/location.png'
-import profile from '../../assets/profile.png'
 import { Api } from '../../utils/api'
+import profile from '../../assets/profile.png'
 
 const isUserDentist = false // should be extended with getting current user entity when we have it on BE
 const routes = isUserDentist ? null : patientRoutes
@@ -50,9 +50,9 @@ export default function Navbar (): JSX.Element {
                     <img class="h-8 w-auto" src={logo} alt="AppointDent" />
                 </a>
                 <div class="hidden md:ml-6 ml-2 sm:block">
-                <div class="flex space-x-0">
+                <div class="flex ">
                     <For each={routes}>{(route, index) =>
-                        <div class="flex row mr-0 ">
+                        <div class="flex row">
                             <a href={route.href} class="rounded-md px-2 md:px-3 py-2 text-sm font-medium">{route.name}</a>
                             {route.name === 'Explore' && <img class="w-6 h-6 mt-1" src={location} alt="Arrow left" />}
                             {index() === 0 && <div class='border-l h-auto ml-2 lg:ml-5 bg-white' style="color: white;"/>}
@@ -61,25 +61,27 @@ export default function Navbar (): JSX.Element {
                 </div>
                 </div>
             </div>
-            <div class="flex items-center pr-0 sm:static sm:inset-auto sm:pr-0">
+                <div class="flex items-center pr-0 sm:static sm:inset-auto ">
+            <a href="/notifications">
                 <button onClick={toggleNotification} type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none">
-                <span class="absolute -inset-1.5"></span>
-                <span class="sr-only">View notifications</span>
-                <svg class="h-6 w-6 notification" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                </svg>
+                    <span class="absolute -inset-1.5"></span>
+                    <span class="sr-only">View notifications</span>
+                    <svg class="h-6 w-6 notification" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                    </svg>
                 </button>
-                <div class="inset-y-0 right-0 absolute top-10">
-                    <Show when={notify()}>
-                        <div class={ fadeIn() ? 'notification fade-in-element' : fadeOut() ? 'notification fade-out-element' : 'notification' }>
-                            <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                                <p class="block px-4 py-2 text-sm text-primary">No notifications!</p>
-                            </div>
+            </a>
+            <div class="inset-y-0 right-0 absolute top-10">
+                <Show when={notify()}>
+                    <div class={fadeIn() ? 'notification fade-in-element' : fadeOut() ? 'notification fade-out-element' : 'notification'}>
+                        <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                            <p class="block px-4 py-2 text-sm text-primary">No notifications!</p>
                         </div>
-                    </Show>
-                </div>
+                    </div>
+                </Show>
             </div>
-            <div class="relative ml-2 md:ml-4">
+        </div>
+        <div class="relative ml-2 md:ml-4">
                 <div>
                     <button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                     <span class="sr-only">Open user menu</span>
@@ -99,20 +101,21 @@ export default function Navbar (): JSX.Element {
                 }}>Log out</button>
             </div>
             </div>
-        </div>
 
-    </nav>
-    <Show when={hamburger()}>
-        <div class={ slideIn() ? 'slide-in-element bg-primary zUnder' : slideOut() ? 'slide-out-element bg-primary zUnder' : 'bg-primary zUnder' }>
-            <div id="">
-                <div class="space-y-1 px-2 pb-3 pt-2">
-                <For each={routes}>{(route) =>
-                        <a href={route.href} class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">{route.name}</a>
+                    </div>
 
-                    }</For>
+            </nav>
+            <Show when={hamburger()}>
+                <div class={ slideIn() ? 'slide-in-element bg-primary zUnder' : slideOut() ? 'slide-out-element bg-primary zUnder' : 'bg-primary zUnder' }>
+                    <div id="">
+                        <div class="space-y-1 px-2 pb-3 pt-2">
+                        <For each={routes}>{(route) =>
+                                <a href={route.href} class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">{route.name}</a>
+
+                            }</For>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        </Show>
-    </>
+                </Show>
+            </>
 }
