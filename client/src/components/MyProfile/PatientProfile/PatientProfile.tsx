90185c9095ad8fddaf8ada73e06887fbdbee3052
OmidKhodaparast2002 <guskhodom@student.gu.se>
2023-12-04 12:25:00 +0100

    #83 client information validation
    I added validation when users patch their profile.

--- client/src/components/MyProfile/PatientProfile/PatientProfile.tsx
+++ client/src/components/MyProfile/PatientProfile/PatientProfile.tsx
@@ -5,7 +5,7 @@ import { createStore } from 'solid-js/store'
 import { createEffect, createSignal, type JSX } from 'solid-js'
 import { type AxiosResponse } from 'axios'
 import { type PatientProfileProps } from '../MyProfileTypes'
-import { isValidPatient } from '../utils'
+import { validateUserInfo } from '../utils'
 import CustomInput from '../CustomInput'
 import { Api } from '../../../utils/api'
 
@@ -18,7 +18,7 @@ export default function PatientProfile (patientProp: PatientProfileProps): JSX.E
   })
 
   const patchPatient = async function patchPatient (patchedPatient: Patient): Promise<void> {
-    const validPatient = isValidPatient(patient)
+    const validPatient = validateUserInfo(patchedPatient)
     if (validPatient !== undefined) {
       setError(new Error(validPatient))
       setTimeout(() => setError(null), 2000)
