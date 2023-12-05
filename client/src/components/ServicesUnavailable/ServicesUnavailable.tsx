79bbad2d002f36ccde141cdf075a92d9adb7fd89
mariiaz <mariiaz@student.chalmers.se>
2023-12-05 22:21:01 +0100

    #91 add page responsiveness and style improvements

--- client/src/components/ServicesUnavailable/ServicesUnavailable.tsx
+++ client/src/components/ServicesUnavailable/ServicesUnavailable.tsx
@@ -4,26 +4,29 @@ import { createEffect, createSignal } from 'solid-js'
 import ServicesUnavailableModal from './ServicesUnavailableModal'
 import { Api } from '../../utils/api'
 
-const servicesErr = async (): Promise<string[]> => {
+const servicesErr = async (): Promise<void> => {
   const response = await Api.get('/heartbeat', { withCredentials: true })
-  return response.data
+  setErrServices(response.data.data)
 }
 
 createEffect(async () => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   setInterval(async () => {
-    setErrServices(await servicesErr())
+    await servicesErr()
   }, 5000)
 })
 
 const [errServices, setErrServices] = createSignal<string[]>([])
 const [showList, setShowList] = createSignal<boolean>(false)
 export default function ServicesUnavailable (): JSX.Element {
-  return <>
-  <div class='servicesContainer flex flex-col rounded p-2 mr-8 cursor-pointer items-center lg:flex-row' onClick={() => setShowList(true)}>
-    <div class='mr-1 text-white'>🚨 Some services are unavailable</div>
-        <strong class='seeMoreText'>...see more</strong>
-        {showList() &&
+  return <> {
+    errServices().length > 0 &&
+    <>
+    <div class='servicesContainer flex flex-col rounded p-2 mr-8 cursor-pointer items-center lg:flex-row' onClick={() => setShowList(true)}>
+    <div class='text-white text-xs lg:mr-1 md:text-base'>🚨 Some services are unavailable</div>
+        <strong class='seeMoreText text-xs md:text-base'>...see more</strong>
+    </div>
+    {showList() &&
         <ServicesUnavailableModal
         onCancel={() => {
           setShowList(false)
@@ -31,6 +34,8 @@ export default function ServicesUnavailable (): JSX.Element {
         services={errServices()}
         />
         }
-    </div>
+    </>
+  }
+
   </>
 }
--- client/src/components/ServicesUnavailable/ServicesUnavailableModal.tsx
+++ client/src/components/ServicesUnavailable/ServicesUnavailableModal.tsx
@@ -1,4 +1,5 @@
 import { For } from 'solid-js'
+import './ServicesUnavailable.css'
 import { type JSX } from 'solid-js/jsx-runtime'
 
 interface ModalProps {
@@ -6,18 +7,21 @@ interface ModalProps {
   services: string[]
 }
 
+const transformString = (input: string): string => {
+  const words = input.split('-')
+  return words.map((word: string) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+}
+
 export default function ServicesUnavailableModal (props: ModalProps): JSX.Element {
   return (
     <div class="fixed inset-0 flex items-center justify-center z-50 text-black bg-transparent-black">
       <div class="bg-white p-10 rounded-lg shadow-xl text-black w-96">
         <p>We're facing technical issues, and the following services are temporarily unavailable:</p>
-        <div class='rounded p-6 pt-2 pb-2 mt-4 mb-4 text-white' style='background-color: rgb(217, 152, 152);'>
+        <div class='servicesContainer rounded p-6 pt-2 pb-2 mt-4 mb-4 text-white'>
           <For each={props.services}>{(item) =>
-              <p class='text-white'>{item}</p>
+              <p class='text-white'>{transformString(item)}</p>
            }
           </For>
-          <p class='text-white'>Login Service</p>
-          <p class='text-white'>Register Service</p>
         </div>
         <p>Our team is actively working to fix the problem. Thanks for your patience!</p>
         <div class="flex justify-end mt-4">
