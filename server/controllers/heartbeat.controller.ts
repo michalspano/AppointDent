16fe1ecffff7761f81bf027a5bea3ce8d042d283
mariiaz <mariiaz@student.chalmers.se>
2023-12-05 22:20:07 +0100

    #91 improve BE response

--- server/controllers/heartbeat.controller.ts
+++ server/controllers/heartbeat.controller.ts
@@ -1,9 +1,8 @@
 import type { Request, Response } from 'express';
-import { panicMonitor } from '../mqtt/heartbeatListener';
+import { killedServices } from '../mqtt/heartbeatListener';
 
 export const heartbeatController = async (req: Request, res: Response): Promise<Response<any, Record<string, any>>> => {
-  console.log('heartbeatController', await panicMonitor(500, 10));
-  return res.status(200).json({ data: await panicMonitor(500, 10) });
+  return res.status(200).json({ data: killedServices });
 };
 
 export const heartbeatWrapper = (req: Request, res: Response): void => {
--- server/mqtt/heartbeatListener.ts
+++ server/mqtt/heartbeatListener.ts
@@ -1,34 +1,27 @@
 import type * as mqtt from 'mqtt';
 
 const heartbeatMonitor: Record<string, number> = {};
+export const killedServices: string[] = [];
 
-export async function panicMonitor (inspectInterval: number, panicThreshold: number): Promise<string[]> {
-  return await new Promise((resolve) => {
-    const killedServices: string[] = [];
-    setInterval(() => {
-      for (const key in heartbeatMonitor) {
-        const diff: number = Math.round(Date.now() / 1000) - heartbeatMonitor[key];
-        if (diff > panicThreshold) {
-          killedServices.push(key);
-          console.log(`${key} flatlined.`);
-          // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-          delete heartbeatMonitor[key];
-        }
+export function panicMonitor (inspectInterval: number, panicThreshold: number): void {
+  setInterval(() => {
+    for (const key in heartbeatMonitor) {
+      const diff: number = Math.round(Date.now() / 1000) - heartbeatMonitor[key];
+      if (diff > panicThreshold) {
+        killedServices.push(key);
+        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+        delete heartbeatMonitor[key];
+        console.error(`${key} flatlined.`);
       }
-      if (killedServices.length > 0) {
-        console.log('killed', killedServices);
-        resolve(killedServices);
-      }
-    }, inspectInterval);
-    resolve(killedServices);
-  });
+    }
+  }, inspectInterval);
 }
 
 export async function listenForHeartbeat (services: string[], client: mqtt.MqttClient, panicThreshold: number): Promise<void> {
   for (let i = 0; i < services.length; i++) {
     heartbeatMonitor[services[i]] = Math.round(Date.now() / 1000);
   }
-  void panicMonitor(500, panicThreshold);
+  panicMonitor(500, panicThreshold);
 
   client?.on('message', (topic: string, message: Buffer) => {
     if (topic === 'HEARTBEAT') {
