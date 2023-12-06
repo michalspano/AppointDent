305fb77bbb5703b64def2f214a2bd766289b38b8
Agrima Singh <agrima@student.chalmers.se>
2023-12-06 10:37:12 +0100

    #80 Refactor code to make it more maintainable

--- client/src/components/Admin/AdminPage.tsx
+++ client/src/components/Admin/AdminPage.tsx
@@ -1,25 +1,34 @@
 import { createSignal, type JSX, Show } from 'solid-js'
 
+export interface Place {
+  tab: string
+  title: string
+}
+
 // To define the Admin component
 export default function Admin (): JSX.Element {
   // To track the active tab
   const [activeTab, setActiveTab] = createSignal('general')
 
+  const tabComponent = (item: Place): JSX.Element => {
+    return (
+        <TabButton tab={item.tab} activeTab={activeTab} setActiveTab={setActiveTab}>
+          {item.title}
+        </TabButton>
+    )
+  }
+  const data = [
+    { tab: 'general', title: 'General' },
+    { tab: 'patients', title: 'Patients' },
+    { tab: 'appointments', title: 'Appointments' },
+    { tab: 'dentists', title: 'Dentists' }
+  ]
   return (
-    <div class="flex flex-col">
-      <div class="flex">
-        <TabButton tab="general" activeTab={activeTab} setActiveTab={setActiveTab}>
-          General
-        </TabButton>
-        <TabButton tab="patients" activeTab={activeTab} setActiveTab={setActiveTab}>
-          Patients
-        </TabButton>
-        <TabButton tab="appointments" activeTab={activeTab} setActiveTab={setActiveTab}>
-          Appointments
-        </TabButton>
-        <TabButton tab="dentists" activeTab={activeTab} setActiveTab={setActiveTab}>
-          Dentists
-        </TabButton>
+    <div class="flex flex-col p-8">
+        <div class="flex items-center">
+      <h2 class="text-2xl font-bold px-2">Statistics</h2>
+        {data.map(item => tabComponent(item))}
+
       </div>
 
       <Show when={activeTab() === 'general'}>
@@ -32,7 +41,9 @@ export default function Admin (): JSX.Element {
         <AppointmentsContent />
       </Show>
       <Show when={activeTab() === 'dentists'}>
+        <div class="flex items-center justify-center mt-8">
         <DentistsContent />
+        </div>
       </Show>
     </div>
   )
@@ -49,7 +60,7 @@ function TabButton (props: {
 
   return (
     <button
-      class="px-4 py-2"
+      class="px-4 py-2 text-black mr-4 text-lg"
       onClick={() => {
         setActiveTab(tab)
       }}
@@ -71,6 +82,9 @@ function AppointmentsContent (): JSX.Element {
   return <div>Appointments Content</div>
 }
 
+// add placeholders
 function DentistsContent (): JSX.Element {
-  return <div>Dentists Content</div>
+  return <div class="h-96 w-4/5 border-2 border-gray-500 rounded-lg relative shadow-lg px-8 py-8 text-gray-500">
+  Graph Component for Dentist
+</div>
 }
