41ac7121db4498790d46749c6f64892470f67692
Agrima Singh <agrima@student.chalmers.se>
2023-12-06 14:39:06 +0100

    #80 Improve styling of the page

--- client/src/components/Admin/AdminPage.tsx
+++ client/src/components/Admin/AdminPage.tsx
@@ -1,15 +1,17 @@
 import { createSignal, type JSX, Show } from 'solid-js'
+import './AdminPage.css'
 
 export interface Place {
   tab: string
   title: string
 }
 
-// To define the Admin component
+// Define the Admin component
 export default function Admin (): JSX.Element {
-  // To track the active tab
+  // State to track the active tab
   const [activeTab, setActiveTab] = createSignal('general')
 
+  // Function to render tab buttons based on the data
   const tabComponent = (item: Place): JSX.Element => {
     return (
         <TabButton tab={item.tab} activeTab={activeTab} setActiveTab={setActiveTab}>
@@ -17,6 +19,8 @@ export default function Admin (): JSX.Element {
         </TabButton>
     )
   }
+
+  // Data for tabs
   const data = [
     { tab: 'general', title: 'General' },
     { tab: 'patients', title: 'Patients' },
@@ -28,17 +32,22 @@ export default function Admin (): JSX.Element {
         <div class="flex items-center">
       <h2 class="text-2xl font-bold px-2">Statistics</h2>
         {data.map(item => tabComponent(item))}
-
       </div>
 
       <Show when={activeTab() === 'general'}>
+      <div class="flex items-center justify-center mt-8">
         <GeneralContent />
+        </div>
       </Show>
       <Show when={activeTab() === 'patients'}>
+      <div class="flex items-center justify-center mt-8">
         <PatientsContent />
+        </div>
       </Show>
       <Show when={activeTab() === 'appointments'}>
+      <div class="flex items-center justify-center mt-8">
         <AppointmentsContent />
+        </div>
       </Show>
       <Show when={activeTab() === 'dentists'}>
         <div class="flex items-center justify-center mt-8">
@@ -58,9 +67,10 @@ function TabButton (props: {
 }): JSX.Element {
   const { tab, setActiveTab, children } = props
 
+  // Custom button styling with an event handler
   return (
     <button
-      class="px-4 py-2 text-black mr-4 text-lg"
+      class="custom-button mr-4"
       onClick={() => {
         setActiveTab(tab)
       }}
@@ -69,22 +79,39 @@ function TabButton (props: {
     </button>
   )
 }
+
 // To replace with graph components
 function GeneralContent (): JSX.Element {
-  return <div>General Content</div>
+  return <GraphComponentContainer>
+    General Graph Component
+    </GraphComponentContainer>
 }
 
 function PatientsContent (): JSX.Element {
-  return <div>Patients Content</div>
+  return <GraphComponentContainer>
+    Graph Component for Patients
+    </GraphComponentContainer>
 }
 
 function AppointmentsContent (): JSX.Element {
-  return <div>Appointments Content</div>
+  return <GraphComponentContainer>
+    Graph Component for Appointments
+    </GraphComponentContainer>
 }
 
 // add placeholders
 function DentistsContent (): JSX.Element {
-  return <div class="h-96 w-4/5 border-2 border-gray-500 rounded-lg relative shadow-lg px-8 py-8 text-gray-500">
-  Graph Component for Dentist
-</div>
+  return <GraphComponentContainer>
+  Graph Component for Dentists
+  </GraphComponentContainer>
+}
+
+// Container component for graph components with styling
+function GraphComponentContainer (props: { children: string }): JSX.Element {
+  const containerClasses = 'h-96 w-4/5 border-2 border-gray-500 rounded-lg relative shadow-lg px-8 py-8 text-gray-500 mt-4 flex items-center justify-center'
+  return (
+      <div class={containerClasses}>
+        {props.children}
+      </div>
+  )
 }
