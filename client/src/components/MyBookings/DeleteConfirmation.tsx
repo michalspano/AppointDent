7ba7270c24c4a2b3eca0fef09fe5000b09ec19a7
arumeel <kaisa.arumeel@gmail.com>
2023-12-11 19:50:26 +0100

    #104 Update cancelling text and some colors

--- client/src/components/MyBookings/DeleteConfirmation.tsx
+++ client/src/components/MyBookings/DeleteConfirmation.tsx
@@ -10,7 +10,7 @@ export default function ConfirmationPopup (props: ConfirmationPopupProps): JSX.E
     <div class="fixed inset-0 flex items-center justify-center z-50 text-white">
       <div class="bg-primary p-10 rounded-lg text-white shadow-xl">
         <p class="delete-text mb-4">
-            Do you want to delete this event?</p>
+            Are you sure you want to cancel this appointment?</p>
         <div class="flex justify-end">
         <button
             class="bg-secondary px-4 py-2 rounded"
