8a3221e3e10d94498fa108ee03cac3c71d73c5d4
spano <spano@student.chalmers.se>
2023-12-19 17:22:16 +0100

    #113 Update patient controllers, refactoring

--- services/patient-service/controllers/login.controller.ts
+++ services/patient-service/controllers/login.controller.ts
@@ -1,7 +1,8 @@
-import type { Request, Response } from 'express';
+import QUERY from '../utils/query';
 import database from '../db/config';
 import { client } from '../mqtt/mqtt';
 import { getServiceResponse } from './helper';
+import type { Request, Response } from 'express';
 
 const TOPIC = 'CREATESESSION';
 const RESPONSE_TOPIC = 'SESSION';
@@ -16,20 +17,14 @@ export const loginController = async (req: Request, res: Response): Promise<Resp
   if (req.body.email === undefined) return res.sendStatus(400);
   if (req.body.password === undefined) return res.sendStatus(400);
 
-  const { email, password } = req.body;
-  if (database === undefined) {
-    return res.sendStatus(500);
-  }
-  if (client === undefined) {
-    return res.sendStatus(500);
-  }
+  const { email, password } = req.body as Record<string, string>;
+  if (database === undefined) return res.sendStatus(500);
+  if (client === undefined) return res.sendStatus(500);
 
   try {
-    const result = database.prepare('SELECT email FROM patients WHERE email = ?').get(req.body.email);
-
-    if (result === undefined) {
-      return res.sendStatus(404);
-    }
+    // The email either exists or it doesn't.
+    const result = QUERY.VERIFY_PATIENT.get(req.body.email) as string | undefined;
+    if (result === undefined) return res.sendStatus(404);
   } catch (err) {
     return res.status(500).json({
       message: 'Internal server error: failed performing selection.'
@@ -42,11 +37,9 @@ export const loginController = async (req: Request, res: Response): Promise<Resp
 
   try {
     const mqttResult = await getServiceResponse(reqId.toString(), RESPONSE_TOPIC);
+    if (mqttResult === '0') return res.sendStatus(401);
 
-    if (mqttResult === '0') {
-      return res.sendStatus(401);
-    }
-
+    // TODO: revisit this logic
     if (mqttResult !== undefined && mqttResult.length === 1 && mqttResult === '0') {
       return res.sendStatus(400);
     } else {
@@ -56,7 +49,7 @@ export const loginController = async (req: Request, res: Response): Promise<Resp
   } catch (error) {
     return res.sendStatus(500);
   } finally {
-    client?.unsubscribe(RESPONSE_TOPIC);
+    client.unsubscribe(RESPONSE_TOPIC);
   }
 };
 /**
--- services/patient-service/controllers/register.controller.ts
+++ services/patient-service/controllers/register.controller.ts
@@ -1,7 +1,10 @@
-import type { Request, Response } from 'express';
+import QUERY from '../utils/query';
 import database from '../db/config';
 import { client } from '../mqtt/mqtt';
 import { getServiceResponse } from './helper';
+import type { Patient } from '../utils/types';
+import type { Request, Response } from 'express';
+
 const TOPIC = 'INSERTUSER';
 const RESPONSE_TOPIC = 'INSERTUSERRES';
 
@@ -12,6 +15,7 @@ const RESPONSE_TOPIC = 'INSERTUSERRES';
  * @returns request object
  */
 export const registerController = async (req: Request, res: Response): Promise<Response<any, Record<string, any>>> => {
+  // TODO: extract this to a function
   if (req.body.email === undefined) return res.sendStatus(400);
   if (req.body.password === undefined) return res.sendStatus(400);
   if (req.body.birthDate === undefined) return res.sendStatus(400);
@@ -19,39 +23,28 @@ export const registerController = async (req: Request, res: Response): Promise<R
   if (req.body.firstName === undefined) return res.sendStatus(400);
 
   const {
-    email,
-    password,
-    birthDate,
-    lastName,
-    firstName
-  } = req.body;
-  // To verify if required dependencies are defined
-  if (database === undefined) {
-    return res.sendStatus(500);
-  }
-  if (client === undefined) {
-    return res.sendStatus(500);
-  }
+    email, birthDate, lastName, firstName
+  } = req.body as Patient;
+  const password: string = req.body.password;
 
-  // To check if the email is already registered
+  if (database === undefined || client === undefined) return res.sendStatus(500);
+
   if (checkEmailRegistered(email)) {
-    return res.status(409).json('Email is already registered.');
+    return res.status(409).json({
+      message: 'Email is already registered.'
+    });
   }
 
-  let query;
+  // Attempt to insert the patient into the database.
   try {
-  // To insert user into the database
-    query = database.prepare(`
-    INSERT INTO patients 
-    (email, birthDate, lastName, firstName) 
-    VALUES (?, ?, ?, ?)
-  `);
-  } catch (err) {
-    console.log(err);
-    return res.sendStatus(400);
+    QUERY.INSERT_PATIENT.run(email, birthDate, lastName, firstName);
+  } catch (error) {
+    return res.sendStatus(500);
   }
 
-  // To generate a random reqId
+  /**
+   * Insertion successful, now publish to MQTT.
+   */
   const reqId = Math.floor(Math.random() * 1000);
 
   // To publish registration information to MQTT topic
@@ -60,31 +53,28 @@ export const registerController = async (req: Request, res: Response): Promise<R
 
   try {
     // To wait for MQTT response
+    // TODO: add types.
     const mqttResult = await getServiceResponse(reqId.toString(), RESPONSE_TOPIC);
     // To handle unsuccessful authorization
     if (mqttResult === '0') {
-      return res.status(401).json('Email is already registered');
+      return res.status(401).json({
+        message: 'Email is already registered'
+      });
     }
   } catch (error) {
     console.error('Error in registerController:', error);
     return res.sendStatus(500);
   }
-  query.run(email, birthDate, lastName, firstName);
 
   return res.sendStatus(201);
 };
 
 // To check if the email is already registered
 function checkEmailRegistered (email: string): boolean {
-  const checkEmailQuery = database?.prepare(`
-    SELECT COUNT(*) as count
-    FROM patients
-    WHERE email = ?
-  `);
-  const emailCheckResult = checkEmailQuery?.get(email) as { count: number };
-
+  const emailCheckResult = QUERY.PATIENT_COUNT.get(email) as { count: number };
   return emailCheckResult !== null && emailCheckResult.count > 0;
 };
+
 /**
  * Wraps the promise returning function to conform to ts constraints
  * @param req request
