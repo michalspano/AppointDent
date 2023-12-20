1255c8bc959b458d1f6d523fdec0a7738799a5ff
alesaf <gussafala@student.gu.se>
2023-12-20 01:25:56 +0100

    #126 - Added loader to sign up and login
    I have added a spinner and loading text on the sign up page and the login page.

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { type JSX } from 'solid-js/jsx-runtime'
-import { createSignal } from 'solid-js'
+import { Show, createSignal } from 'solid-js'
 import './LoginForm.css'
 import logo from '../../assets/logo.png'
 import { A } from '@solidjs/router'
@@ -9,9 +9,10 @@ export default function LoginForm (): JSX.Element {
   const [email, setEmail] = createSignal('')
   const [password, setPassword] = createSignal('')
   const [error, setError] = createSignal<string | null>(null)
-
+  const [awaitingResponse, setAwaitingResponse] = createSignal(false)
   const login = async (): Promise<void> => {
     try {
+      setAwaitingResponse(true)
       // Login as a dentist
       const response = await Api.post('/dentists/login', { email: email(), password: password() }, { withCredentials: true })
       console.log(response)
@@ -29,7 +30,7 @@ export default function LoginForm (): JSX.Element {
         }
       } catch (patientError) {
         try {
-        // If dentist and patient login fails, admin login is attempted
+          // If dentist and patient login fails, admin login is attempted
           const response = await Api.post('/admins/login', { email: email(), password: password() }, { withCredentials: true })
           // If admin login is successful, user is redirected to admin-specific page
           if (response.status === 200) {
@@ -38,25 +39,26 @@ export default function LoginForm (): JSX.Element {
         } catch (error) { // Error handling of both patient and dentist login fails
           console.error('Error during login', error)
           setError('Login failed. Please check your credentials and try again.')
+          setAwaitingResponse(false)
         }
       }
     }
   }
 
   return <>
-  <div class="h-full w-full bg-white flex flex-col items-center justify-center">
-        <div class="lg:w-3/4 w-5/6 flex flex-col text-black rounded-sm bg-gradient-to-b from-neutral ... px-10 py-10 text-sm font-medium">
-            <div class="flex items-center justify-center">
-                <img class="w-40 " src={logo} alt="AppointDent" />
-            </div>
-            <h1 class="mb-4 mt-10 text-lg">Log in</h1>
-            <p class="mb-10 font-extralight">Hello, welcome back to AppointDent!</p>
-          <input
+    <div class="h-full w-full bg-white flex flex-col items-center justify-center">
+      <div class="lg:w-3/4 w-5/6 flex flex-col text-black rounded-sm bg-gradient-to-b from-neutral ... px-10 py-10 text-sm font-medium">
+        <div class="flex items-center justify-center">
+          <img class="w-40 " src={logo} alt="AppointDent" />
+        </div>
+        <h1 class="mb-4 mt-10 text-lg">Log in</h1>
+        <p class="mb-10 font-extralight">Hello, welcome back to AppointDent!</p>
+        <input
           class="input h-12 px-3 py-2 mb-5 border rounded-xl"
           type="text"
           placeholder="Email"
           onChange={(event) => setEmail(event.target.value)}
-          />
+        />
         <input
           class="input h-12 px-3 py-2 mb-8 border rounded-xl"
           type="password"
@@ -65,20 +67,25 @@ export default function LoginForm (): JSX.Element {
         />
         {error() !== null && <p class="text-error">{error()}</p>}
         <button type="submit" class="log-in-btn h-12 mb-10 bg-secondary rounded-xl text-base"
-        onclick={() => {
-          login()
-            .catch((error) => {
-              console.error('Error creating account:', error)
-            })
-        }}>
-            Log in
-            </button>
+          onclick={() => {
+            login()
+              .catch((error) => {
+                console.error('Error creating account:', error)
+              })
+          }}>
+          <Show when={awaitingResponse()} fallback={'Log in'}>
+            <div class="flex gap-2 justify-center items-center ">
+              <div class="border-2 border-t-primary border-b-white border-l-white border-r-white rounded-lg w-4 h-4 animate-spin"></div>
+              <span>Loading</span>
+            </div>
+          </Show>
+        </button>
         <p class="font-extralight">Not registered yet?
-        <span class="font-medium">
-        <A class='text-black' href="/signup"> Create an account.</A>
-            </span>
-          </p>
-        </div>
+          <span class="font-medium">
+            <A class='text-black' href="/signup"> Create an account.</A>
+          </span>
+        </p>
       </div>
-    </>
+    </div>
+  </>
 }
